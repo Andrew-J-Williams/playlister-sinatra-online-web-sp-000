c899eef184809fa1aa51bcc0968c0170ad24e0f0
Andrew Williams <1williamsaj@gmail.com>
2020-01-28 05:44:39 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -34,12 +34,12 @@ class SongsController < ApplicationController
     redirect("/songs/#{@song.slug}")
   end
 
-  patch '/songs/:slug' do
+  patch '/songs/:slug' do #Essentially does the same thing as 'new', but instead updates just the artist name or genre.
     @song = Song.find_by_slug(params[:slug])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.genre_ids = params[:genres]
     @song.save
-    
+
     flash[:message] = "Successfully updated song."
     redirect("/songs/#{@song.slug}")
   end
