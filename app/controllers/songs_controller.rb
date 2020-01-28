0d4e97ec809ef3604fb129d1e4a8c1f17ca56695
Andrew Williams <1williamsaj@gmail.com>
2020-01-28 04:47:53 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,6 +17,7 @@ class SongsController < ApplicationController
   post '/songs' do
     @song = Song.create(params[:song])
     @song.artist = Artist.find_or_create_by(:artist => params["Artist Name"])
+    @song.genre = params["Genre"]
 
     redirect to "/songs/#{@song.name.slug}"
   end
