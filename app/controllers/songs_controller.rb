cf1d011ddb1078f953b856e7f7eaa6e55f24da01
Andrew Williams <1williamsaj@gmail.com>
2020-01-28 05:04:29 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,10 +20,7 @@ class SongsController < ApplicationController
 
   post '/songs' do
     @song = Song.create(params[:song])
-    #@song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-    if !params[:artist][:name].empty?
-      @song.artist = Artist.create(name: params[:artist][:name])
-    end
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.genre_ids = params[:genres]
     @song.save
 
