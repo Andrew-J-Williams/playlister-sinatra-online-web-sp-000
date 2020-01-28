e30f686ff2770219c9caebc406a4ccc817ab603b
Andrew Williams <1williamsaj@gmail.com>
2020-01-28 05:14:30 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,7 +19,7 @@ class SongsController < ApplicationController
   end
 
   get '/songs/:slug/edit' do
-    @song = Song.find(params[:slug])
+    @song = Song.find_by_slug(params[:slug])
 
     erb :'/songs/edit'
   end
