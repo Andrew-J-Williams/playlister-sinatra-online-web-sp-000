864e87e8b97c0c9dcdf43801e8db97fa0daa0495
Andrew Williams <1williamsaj@gmail.com>
2020-01-28 02:51:56 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,16 @@
 class Genre < ActiveRecord::Base
+
   has_many :songs
   has_many :song_genres
   has_many :artists, through: :songs
   has_many :songs, through: :song_genres
+
+  def slug
+    self.name.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{|name| name.slug == slug}
+  end
+  
 end
