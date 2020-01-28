254ecbde03e523ee22de0eec06d087c840e016d0
Andrew Williams <1williamsaj@gmail.com>
2020-01-28 05:06:27 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,11 +20,11 @@ class SongsController < ApplicationController
 
   post '/songs' do
     @song = Song.create(params[:song])
-        @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-        @song.genre_ids = params[:genres]
-        @song.save
-        flash[:message] = "Successfully created song."
-        redirect("/songs/#{@song.slug}")
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save
+    flash[:message] = "Successfully created song."
+    redirect("/songs/#{@song.slug}")
   end
 
 end
